d58b2f98c05932b9c93e8ca8fa0a7297c8c722e3
Chaeho Yu <chaeho1234@naver.com>
2025-07-05 08:03:22 +0900

    first commit

---
+++ src/pages/Home.tsx
@@ -0,0 +1,16 @@
+import './home.scss';
+
+
+export default function Home() {
+  return (
+    <div className="layout-container" >
+      <a className='name' href='https://github.com/GonGe1018' target='_blank' rel='noreferrer'>
+
+        <div className='letter'> <div className='letter-kor'>유</div> <div className='letter-jp'>柳</div> </div>
+        <div className='letter'> <div className='letter-kor'>채</div> <div className='letter-jp'>采</div> </div>
+        <div className='letter'> <div className='letter-kor'>호</div> <div className='letter-jp'>澔</div> </div>
+
+      </a>
+    </div>
+  );
+}
